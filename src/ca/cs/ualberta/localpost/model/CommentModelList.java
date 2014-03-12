b75325f2671dbbd70aba8f33a7e7d98009df74ef
afroholic78 <mickael.zerihoun@gmail.com>
2014-03-11 19:09:26 -0600

    Comment Model List
    Feels like we'll need a Model List, we add comments to the model list,
    pull the list from server if it exists, delete it and re-add it to the
    list to the server.

---
+++ src/ca/cs/ualberta/localpost/model/CommentModelList.java
@@ -0,0 +1,12 @@
+package ca.cs.ualberta.localpost.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class CommentModelList {
+
+	private ArrayList<CommentModel> list;
+	
+}
