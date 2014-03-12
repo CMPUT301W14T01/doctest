766b92e7dbc34d4c6913b7ebe4ceae7ebf1bc67c
afroholic78 <mickael.zerihoun@gmail.com>
2014-03-11 19:52:49 -0600

    Modified Comment Model List
    Added constructor that adds a parent comment to the list
    Added function to add child comments to said list

--- src/ca/cs/ualberta/localpost/model/CommentModelList.java
+++ src/ca/cs/ualberta/localpost/model/CommentModelList.java
@@ -5,8 +5,23 @@ import java.util.List;
 
 
 
+import android.graphics.Bitmap;
+
+
+
 public class CommentModelList {
 
 	private ArrayList<CommentModel> list;
 	
+	public CommentModelList(String content, android.location.Location location, Bitmap picture) {
+		this.list = new ArrayList<CommentModel>();
+		CommentModel new_root = new RootCommentModel(content, location, picture);
+		list.add(new_root);
+		//TODO send new list to elastic search?
+	}
+	
+	public void AddchildComment(String content, android.location.Location location, Bitmap picture){
+		CommentModel new_comment = new ChildCommentModel(content, location, picture);
+		list.add(new_comment);
+	}
 }
