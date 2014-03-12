4498eb083b49132f531b39bd01e4aeef4365b850
afroholic78 <mickael.zerihoun@gmail.com>
2014-03-12 15:22:13 -0600

    Added constructors for text only

--- src/ca/cs/ualberta/localpost/model/RootCommentModel.java
+++ src/ca/cs/ualberta/localpost/model/RootCommentModel.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 
 public class RootCommentModel extends CommentModel {
 
+	public RootCommentModel(String content) {
+		super(content);
+	}
+	
 	public RootCommentModel(String content,android.location.Location location, Bitmap picture) {
 		super(content, location, picture);
 	}
